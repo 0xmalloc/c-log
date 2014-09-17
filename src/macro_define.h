9bcb48efe28b6964ea6663d6c62b70e24fc9060f
Benjamin <wangkun_mr@163.com>
2014-09-17 12:02:23 +0800

    修改宏名称为大写
    一般宏定义的名称都是大写，这样方便用户明白

--- src/macro_define.h
+++ src/macro_define.h
@@ -2,31 +2,31 @@
 #define  _MACRO_DEFINE
 //============basic===================
 
-#define log_error(log_fmt, log_arg...) \
+#define LOG_ERROR(log_fmt, log_arg...) \
     do{ \
         WARN_W.log(LL_ERROR,   "[%s:%d][%s] " log_fmt "\n", \
                      __FILE__, __LINE__, __FUNCTION__, ##log_arg); \
     } while (0) 
 
-#define log_warn(log_fmt, log_arg...) \
+#define LOG_WARN(log_fmt, log_arg...) \
     do{ \
         WARN_W.log(LL_WARNING,   "[%s:%d][%s] " log_fmt "\n", \
                      __FILE__, __LINE__, __FUNCTION__, ##log_arg); \
     } while (0) 
 
-#define log_notice(log_fmt, log_arg...) \
+#define LOG_NOTICE(log_fmt, log_arg...) \
     do{ \
         INFO_W.log(LL_NOTICE,   "[%s:%d][%s] " log_fmt "\n", \
                      __FILE__, __LINE__, __FUNCTION__, ##log_arg); \
     } while (0) 
 
-#define log_trace(log_fmt, log_arg...) \
+#define LOG_TRACE(log_fmt, log_arg...) \
     do{ \
         INFO_W.log(LL_TRACE,   "[%s:%d][%s] " log_fmt "\n", \
                      __FILE__, __LINE__, __FUNCTION__, ##log_arg); \
     } while (0) 
 
-#define log_debug(log_fmt, log_arg...) \
+#define LOG_DEBUG(log_fmt, log_arg...) \
     do{ \
         INFO_W.log(LL_DEBUG,   "[%s:%d][%s] " log_fmt "\n", \
                      __FILE__, __LINE__, __FUNCTION__, ##log_arg); \
@@ -41,13 +41,13 @@
 
 #define MACRO_WARN(condition, log_fmt, log_arg...) {\
     if (condition) {\
-        log_warn( log_fmt, ##log_arg);\
+        LOG_WARN( log_fmt, ##log_arg);\
     }\
 }
 
 #define MACRO_WARN_RET(condition, return_val, log_fmt, log_arg...) {\
     if ((condition)) {\
-        log_warn( log_fmt, ##log_arg);\
+        LOG_WARN( log_fmt, ##log_arg);\
 		return return_val;\
     }\
 }
